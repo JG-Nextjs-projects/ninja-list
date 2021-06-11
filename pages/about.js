9dda651972575c0cde855b3dec371446d53a2db6
Jose Guerra <jose.guerra@dodsgroup.com>
2021-06-11 11:09:28 +0100

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda tempore vero, dolore ducimus ab modi quia molestiae voluptatum? Quae voluptatibus suscipit est earum. Assumenda hic numquam reprehenderit architecto illum totam!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda tempore vero, dolore ducimus ab modi quia molestiae voluptatum? Quae voluptatibus suscipit est earum. Assumenda hic numquam reprehenderit architecto illum totam!</p>
+      </div>
+    </>
+  );
+}
+
+export default About;
